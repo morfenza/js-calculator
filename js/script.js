c9912324fdeffbe6190b243ac7bdd97646fe5d32
Morfenza <ianlourenco@protonmail.com>
2023-01-08 22:57:17 -0300

    Create operation selector function

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ function divide(a, b) {
   if ((a == 0) || (b == 0)) return NaN;
   return a / b;
 }
+
+function operate(a, b, func) {
+  switch (func) {
+    case 'add':
+      return add(a, b);
+
+    case 'subtract':
+      return subtract(a, b);
+    
+    case 'multiply':
+      return multiply(a, b);
+
+    case 'divide':
+      return divide(a, b);
+  }
+}
