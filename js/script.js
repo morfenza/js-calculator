4120ace4deebb550dead06e6d8380c9c1087d6af
Morfenza <ianlourenco@protonmail.com>
2023-01-12 16:17:45 -0300

    Create numbers selectors & event listeners to update value on screen

--- js/script.js
+++ js/script.js
@@ -30,3 +30,68 @@ function operate(a, b, func) {
       return divide(a, b);
   }
 }
+
+let displayValue = 0;
+
+const number9 = document.querySelector('.nine');
+number9.addEventListener('click', () => {
+  displayValue = 9;
+  bottomScreen.textContent = displayValue
+});
+
+const number8 = document.querySelector('.eight');
+number8.addEventListener('click', () => {
+  displayValue = 8;
+  bottomScreen.textContent = displayValue
+});
+
+const number7 = document.querySelector('.seven');
+number7.addEventListener('click', () => {
+  displayValue = 7;
+  bottomScreen.textContent = displayValue
+});
+
+const number6 = document.querySelector('.six');
+number6.addEventListener('click', () => {
+  displayValue = 6;
+  bottomScreen.textContent = displayValue
+});
+
+const number5 = document.querySelector('.five');
+number5.addEventListener('click', () => {
+  displayValue = 5;
+  bottomScreen.textContent = displayValue
+});
+
+const number4 = document.querySelector('.four');
+number4.addEventListener('click', () => {
+  displayValue = 4;
+  bottomScreen.textContent = displayValue
+});
+
+const number3 = document.querySelector('.three');
+number3.addEventListener('click', () => {
+  displayValue = 3;
+  bottomScreen.textContent = displayValue
+});
+
+const number2 = document.querySelector('.two');
+number2.addEventListener('click', () => {
+  displayValue = 2;
+  bottomScreen.textContent = displayValue
+});
+
+const number1 = document.querySelector('.one');
+number1.addEventListener('click', () => {
+  displayValue = 1;
+  bottomScreen.textContent = displayValue
+});
+
+const number0 = document.querySelector('.zero');
+number0.addEventListener('click', () => {
+  displayValue = 0;
+  bottomScreen.textContent = displayValue
+});
+
+const bottomScreen = document.querySelector('.bottom-screen');
+
