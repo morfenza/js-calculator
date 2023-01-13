99a339b579af32a9e3544a6f8aa649735b4e5e69
Morfenza <ianlourenco@protonmail.com>
2023-01-13 17:54:06 -0300

    Simplify and clean the code && add clear and backspace logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /* Functions */
-
 function add(a, b) {
   return a + b;
 }
@@ -19,180 +18,158 @@ function divide(a, b) {
 
 function operate(a, b, func) {
   switch (func) {
-    case 'add':
+    case '+':
       return add(a, b);
 
-    case 'subtract':
+    case '-':
       return subtract(a, b);
     
-    case 'multiply':
+    case 'x':
       return multiply(a, b);
 
-    case 'divide':
+    case '/':
       return divide(a, b);
   }
 }
 
+function backspace() {
+  curValue = curValue.slice(0, -1);
+  bottomScreen.textContent = `${curValue}`;
+}
+
+function clear() {
+  curValue = '';
+  curOperator = '';
+  upperScreen.textContent = '';
+  bottomScreen.textContent = '';
+  curOperator = ''
+  operatorPressed = false;
+  displayResult = false;
+  secondValue = 0;
+  firstValue = 0;
+}
+
+function numberClick(n) {
+  if (operatorPressed === true) {
+    displayResult = true
+  }
+  curValue += n;
+  bottomScreen.textContent += n;
+}
+
+function operatorClick() {
+  if (operatorPressed === false) {
+    // Operator wasn't pressed
+    firstValue = Number(curValue);
+    upperScreen.textContent = `${firstValue} ${curOperator}`;
+    operatorPressed = true;
+    curValue = ''
+    bottomScreen.textContent = ''
+
+  } else if (operatorPressed === true && displayResult === false) {
+    // Operator pressed but the second number wasn't pressed yet
+    upperScreen.textContent = `${firstValue} ${curOperator}`;
+  }
+}
+
+function equalsClick() {
+  if (operatorPressed === true && displayResult === true) {
+    // Operator pressed and second number also pressed
+    if (secondValue !== '') {
+      secondValue = Number(curValue)
+      firstValue = operate(firstValue, secondValue, curOperator);
+      upperScreen.textContent = `${firstValue} ${curOperator}`
+      curValue = ''
+      bottomScreen.textContent = ''
+    }
+  } 
+}
+/* --------- */
+
 /* Aux variables */
 let curValue = '';
+let curOperator = '';
 let firstValue = 0;
 let secondValue = 0;
 let operatorPressed = false;
 let displayResult = false;
+/* --------- */
 
 /* Calculator screens */
 const upperScreen = document.querySelector('.upper-screen');
 const bottomScreen = document.querySelector('.bottom-screen');
+/* --------- */
+
+/* Misc buttons */
+const clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', clear)
+
+const deleteButton = document.querySelector('.delete')
+deleteButton.addEventListener('click', backspace)
+/* --------- */
 
 /* Operator buttons */
 const plusSign = document.querySelector('.plus');
 plusSign.addEventListener('click', () => {
-  if (operatorPressed === false) {
-    // Operator wasn't pressed
-    firstValue = Number(curValue);
-    upperScreen.textContent = `${firstValue} + `;
-    operatorPressed = true;
-
-  } else if (operatorPressed === true && displayResult === false) {
-    // Operator pressed but the second number wasn't pressed yet
-    upperScreen.textContent = `${firstValue} + `;
-
-  } else if (operatorPressed === true && displayResult === true) {
-    // Operator pressed and second number also pressed
-    if (secondValue !== '') {
-      operate(firstValue, secondValue, '+');
-      operatorPressed = false;
-    } 
-  }
-
+  curOperator = '+';
+  operatorClick()
+  equalsClick();
 });
 
 const minusSign = document.querySelector('.minus');
 minusSign.addEventListener('click', () => {
-  if (operatorPressed === false) {
-    // Operator wasn't pressed
-    firstValue = Number(curValue);
-    upperScreen.textContent = `${firstValue} - `;
-    operatorPressed = true;
-
-  } else if (operatorPressed === true && displayResult === false) {
-    // Operator pressed but the second number wasn't pressed yet
-    upperScreen.textContent = `${firstValue} - `;
-
-  } else if (operatorPressed === true && displayResult === true) {
-    // Operator pressed and second number also pressed
-    if (secondValue !== '') {
-      operate(firstValue, secondValue, '-');
-      operatorPressed = false;
-    } 
-  }
+  curOperator = '-';
+  operatorClick()
+  equalsClick();
 });
 
 const multiplySign = document.querySelector('.multiply');
 multiplySign.addEventListener('click', () => {
-  if (operatorPressed === false) {
-    // Operator wasn't pressed
-    firstValue = Number(curValue);
-    upperScreen.textContent = `${firstValue} x `;
-    operatorPressed = true;
-
-  } else if (operatorPressed === true && displayResult === false) {
-    // Operator pressed but the second number wasn't pressed yet
-    upperScreen.textContent = `${firstValue} x `;
-
-  } else if (operatorPressed === true && displayResult === true) {
-    // Operator pressed and second number also pressed
-    if (secondValue !== '') {
-      operate(firstValue, secondValue, 'x');
-      operatorPressed = false;
-    } 
-  }
+  curOperator = 'x';
+  operatorClick()
+  equalsClick();
 });
 
 const divideSign = document.querySelector('.divide')
 divideSign.addEventListener('click', () => {
-  if (operatorPressed === false) {
-    // Operator wasn't pressed
-    firstValue = Number(curValue);
-    upperScreen.textContent = `${firstValue} / `;
-    operatorPressed = true;
-
-  } else if (operatorPressed === true && displayResult === false) {
-    // Operator pressed but the second number wasn't pressed yet
-    upperScreen.textContent = `${firstValue} / `;
-
-  } else if (operatorPressed === true && displayResult === true) {
-    // Operator pressed and second number also pressed
-    if (secondValue !== '') {
-      operate(firstValue, secondValue, '/');
-      operatorPressed = false;
-    } 
-  }
+  curOperator = '/';
+  operatorClick()
+  equalsClick();
 });
 
-const equals = document.querySelector('.equals')
-equals.addEventListener('click', () => {
+const equals = document.querySelector('.equals');
+equals.addEventListener('click', equalsClick);
+/* --------- */
 
-});
 
 /* Number buttons */
 const number9 = document.querySelector('.nine');
-number9.addEventListener('click', () => {
-  curValue += '9';
-  bottomScreen.textContent += '9'
-});
+number9.addEventListener('click', () => numberClick('9'));
 
 const number8 = document.querySelector('.eight');
-number8.addEventListener('click', () => {
-  curValue += '8';
-  bottomScreen.textContent += '8'
-});
+number8.addEventListener('click', () => numberClick('8'));
 
 const number7 = document.querySelector('.seven');
-number7.addEventListener('click', () => {
-  curValue += '7';
-  bottomScreen.textContent += '7'
-});
+number7.addEventListener('click', () => numberClick('7'));
 
 const number6 = document.querySelector('.six');
-number6.addEventListener('click', () => {
-  curValue += '6';
-  bottomScreen.textContent += '6'
-});
+number6.addEventListener('click', () => numberClick('6'));
 
 const number5 = document.querySelector('.five');
-number5.addEventListener('click', () => {
-  curValue += '5';
-  bottomScreen.textContent += '5'
-});
+number5.addEventListener('click', () => numberClick('5'));
 
 const number4 = document.querySelector('.four');
-number4.addEventListener('click', () => {
-  curValue = '4';
-  bottomScreen.textContent += '4'
-});
+number4.addEventListener('click', () => numberClick('4'));
 
 const number3 = document.querySelector('.three');
-number3.addEventListener('click', () => {
-  curValue += '3';
-  bottomScreen.textContent += '3'
-});
+number3.addEventListener('click', () => numberClick('3'));
 
 const number2 = document.querySelector('.two');
-number2.addEventListener('click', () => {
-  curValue += '2';
-  bottomScreen.textContent += '2'
-});
+number2.addEventListener('click', () => numberClick('2'));
 
 const number1 = document.querySelector('.one');
-number1.addEventListener('click', () => {
-  curValue += '1';
-  bottomScreen.textContent += '1'
-});
+number1.addEventListener('click', () => numberClick('1'));
 
 const number0 = document.querySelector('.zero');
-number0.addEventListener('click', () => {
-  curValue += '0';
-  bottomScreen.textContent += '0'
-});
-
+number0.addEventListener('click', () => numberClick('0'));
+/* --------- */
